6987049525d920cefd54d7a67b7522c2c8cbfa2b
Łukasz Urbański <lukasz.urbanski@yahoo.pl>
2019-07-20 03:13:41 +0200

    Dodaj pliki projektów.

---
+++ WpfApp1/BusinessLogic.cs
@@ -0,0 +1,97 @@
+﻿using AcmeCorp.Training.Services;
+using System.Text.RegularExpressions;
+
+namespace WpfApp1
+{
+    public class BusinessLogic
+    {
+        public LegacyObjectMetadataProvider.LatestVersionProvider metadataProvider = new LegacyObjectMetadataProvider.LatestVersionProvider();
+        public ObjectCodeValidator validator = new ObjectCodeValidator();
+
+        public string GetCode(string metadata)
+        {
+            if (!(metadata.StartsWith("<") || metadata.StartsWith("<")))
+            {
+                string[] arrayToBeChecked = metadata.Split(new char[] { '~', '_' });
+                switch (arrayToBeChecked.Length)
+                {
+                    case (6):
+                        return arrayToBeChecked[4];
+                    case (7):
+                        return arrayToBeChecked[5];
+                    case (9):
+                        if (arrayToBeChecked[5].Substring(0, 1).Equals("v"))
+                        {
+                            return arrayToBeChecked[7];
+                        }
+                        else
+                        {
+                            return arrayToBeChecked[5];
+                        }
+                    case (10):
+                        return arrayToBeChecked[6];
+                }
+            }
+            if (metadata.StartsWith("<Object>"))
+            {
+                if (metadata.Contains("<Code>"))
+                {
+                    //ReturnCodeFromMetadataBasedOnPattern(metadata, @"(.*)(<Code>)(.*)(</Code>)(.*)");
+                    string patternCode = @"(.*)(<Code>)(.*)(<\/Code>)(.*)";
+                    string patternMarket = @"(.*)(<Market>)(.*)(<\/Market>)(.*)";
+                    MatchCollection matchesCode = Regex.Matches(metadata, patternCode);
+                    MatchCollection matchesMarket = Regex.Matches(metadata, patternMarket);
+                    foreach (Match matchM in matchesMarket)
+                    {
+                        if (matchM.Groups[3].Value.Equals("PL") || matchM.Groups[3].Value.Equals("BG") || matchM.Groups[3].Value.Equals("EL"))
+                        {
+                            foreach (Match matchC in matchesCode)
+                            {
+                                return matchC.Groups[3].Value.Substring(0, matchC.Groups[3].Value.Length - 2);
+                            }
+                        }
+                        else
+                        {
+                            foreach (Match matchC in matchesCode)
+                            {
+                                return matchC.Groups[3].Value;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    //ReturnCodeFromMetadataBasedOnPattern(metadata, @"(.*)(_v\d~)(.*?(?=~))(.*)");
+                    string pattern = @"(.*)(_v\d~)(.*?(?=~))(.*)";
+                    MatchCollection matches = Regex.Matches(metadata, pattern);
+                    foreach (Match match in matches)
+                    {
+                        return match.Groups[3].Value;
+                    }
+                }
+            }
+            else
+            {
+                //ReturnCodeFromMetadataBasedOnPattern(metadata, @"(.*)(_v\d~)(.*?(?=~))(.*)");
+                string pattern = @"(.*)(_v\d~)(.*?(?=~))(.*)";
+                MatchCollection matches = Regex.Matches(metadata, pattern);
+                foreach (Match match in matches)
+                {
+                    return match.Groups[3].Value;
+                }
+            }
+            return "ERROR";
+        }
+        
+        //Have tried to implement method below to follow DRY rule but it didn't work...
+        private static string ReturnCodeFromMetadataBasedOnPattern(string metadata, string pattern)
+        {
+            MatchCollection matches = Regex.Matches(metadata, pattern);
+            foreach (Match match in matches)
+            {
+                return match.Groups[3].Value;
+            }
+            return "";
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.RunWorkerCompleted += RunWorkerCompleted;
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += WorkerDoWork;
+            worker.ProgressChanged += WorkerProgressChanged;
+            worker.RunWorkerAsync();
+        }
+
+        private void WorkerProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            StartButton.IsEnabled = false;
+            ProgressBar.Value = e.ProgressPercentage;
+            string[] array = (string[])e.UserState;
+            MetadataTextBox.Text = array[0];
+            CodeTextBox.Text = array[1];
+        }
+
+        private void WorkerDoWork(object sender, DoWorkEventArgs e)
+        {
+            //thisWorker.ReportProgress(0, String.Format("XXX 1"));
+            BusinessLogic businessLogic = new BusinessLogic();
+            var thisWorker = sender as BackgroundWorker;
+
+            // How to change 1 000 000 to CounterTextBox.Value?
+
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                if (i % 1000 == 0)
+                {
+                    Thread.Sleep(10);
+                    string metadata = businessLogic.metadataProvider.ProvideMetadata();
+                    string code = businessLogic.GetCode(metadata);
+                    businessLogic.validator.AssertCodeIsValid(code, metadata);
+                    thisWorker.ReportProgress((i + 1) / 10000, new string[] { metadata, code });
+                }                
+            }
+        }
+        //thisWorker.ReportProgress(100, "XXX na 100%");
+
+        private void RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            ProgressBar.Value = 100;
+            MessageBox.Show("Test completed!");
+            ProgressBar.Value = 0;
+            MetadataTextBox.Text = String.Empty;
+            StartButton.IsEnabled = true;
+        }
+    }
+}
